8e8b574b1d13a78f2e9075b10f6e1690e31c492a
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-03-16 12:45:28 -0500

    Add ability to remove a product's picture. Paperclip will render a default picture when that is the case

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,31 @@
 class Product < ActiveRecord::Base
 
+
+
   belongs_to :category
+
+  attr_accessor :image_delete
 
   has_attached_file :photo, :styles => {:small => "150x150>"},
                     :url => "/assets/products/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+                    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension",
+                    :default_url => "/images/no_picture.png"
 
-  validates_attachment_presence :photo
+ # has_destroyable_file :photo
+
+  #validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
   validates_presence_of :category
+
+  before_save { delete_photo if image_delete == '1' }
+
+  private
+
+  def delete_photo
+    self.photo.clear
+  end
+
 
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( ckeditor/* )
 Rails.application.config.assets.precompile += %w( admin/* )
 
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
