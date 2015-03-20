88083a61309fc7cd7d3f4fb761b0d24ec0767be4
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-03-20 11:17:29 -0500

    Categories listing the products within it (without paginating yet)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,25 +1,29 @@
 class CategoriesController < ApplicationController
 
-
+  add_breadcrumb "Categories", :categories_path
 
   # GET /categories
   # GET /categories.json
   def index
-    add_breadcrumb "Categories", link_to: :categories
+    # add_breadcrumb "Categories", link_to: :categories
     @categories = Category.all
   end
 
   # GET /categories/1
   # GET /categories/1.json
   def show
-    add_breadcrumb "Categories", categories_path
+
+
+
     @category = Category.find(params[:id])
     path = @category.path.from_depth(1)
     path.each do |category|
       add_breadcrumb category.name,  category
     end
 
-    @products = Product.where(category: @category)
+    #TODO Verify if the subtree is working properly
+    category_tree = @category.subtree
+    @products = Product.where(category: category_tree)
   end
 
   # GET /categories/new
