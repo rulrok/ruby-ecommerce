2480d399bb783ccfb35ba4c6051452dbb8118ade
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-03-27 09:11:08 -0500

    Listing for products being shown by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,8 +15,9 @@ class CategoriesController < ApplicationController
     end
 
     # TODO Verify if the subtree is working properly
+    page = params[:page].to_i
     category_tree = @category.subtree
-    @products = Product.where(category: category_tree, product_available: true)
+    @products = Product.where(category: category_tree, product_available: true).page(page).per(7)
   end
 
   # GET /categories/new
